73cf8284ca653f1bc32796cd2ec941aa67665d3f
developersrinu <132943635+developersrinu@users.noreply.github.com>
2023-06-28 17:25:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-//your JS code here. If required.
+
+  // JavaScript Code
+  async function showMessage() {
+    const textInput = document.getElementById("text").value;
+    const delayInput = document.getElementById("delay").value;
+
+    // Convert delayInput to a number
+    const delay = parseInt(delayInput);
+
+    // Wait for the specified delay
+    await new Promise(resolve => setTimeout(resolve, delay));
+
+    // Display the text on the screen
+    const output = document.getElementById("output");
+    output.innerText = textInput;
+  }
+
+  // Add event listener to the button
+  document.getElementById("btn").addEventListener("click", showMessage);
+	
